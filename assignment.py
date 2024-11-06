27f5351ebcf047b3ede1ebd031a03000646da4a2
Daniel-Simoes2009 <daniel.simoes@stu.ocsb.ca>
2024-11-06 02:15:23 +0000

    commit2

--- assignment.py
+++ assignment.py
@@ -3,12 +3,27 @@ Sizes = {'L': 6.00, 'XL': 10.00}
 Toppings = {1: 1.00, 2: 1.75, 3: 2.50, 4: 3.35}
 Hst_rate = 0.13
 
-size = input("Pizza size (L/XL): ").upper()
-toppings = int(input("Number of toppings (1-4): "))
+# Get valid size input
+while True:
+    size = input("Pizza size (L/XL): ").upper()
+    if size in Sizes:
+        break
+    print("Invalid size! Please enter L or XL")
 
+# Get valid number of toppings
+while True:
+    try:
+        toppings = int(input("Number of toppings (1-4): "))
+        if 1 <= toppings <= 4:
+            break
+        print("Please enter a number between 1 and 4")
+    except ValueError:
+        print("Please enter a valid number")
 
 subtotal = Sizes[size] + Toppings[toppings]
 tax = subtotal * Hst_rate
 total = subtotal + tax
 
-print(subtotal)
+print(f"Subtotal: ${subtotal:.2f}")
+print(f"Tax: ${tax:.2f}")
+print(f"Total: ${total:.2f}")
--- handler.php
+++ handler.php
@@ -1,23 +1,44 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Welcome Page</title>
+</head>
+
+<body>
+
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $size = $_POST['size'];
+    $toppings = $_POST['toppings'];
+
+    $base = ($size == 'L') ? 6.00 : 10.00;
+
+    $topping_prices = [
+      1 => 1.00,
+      2 => 1.75,
+      3 => 2.50,
+      4 => 3.35
+    ];
+
+    $subtotal = $base + $topping_prices[$toppings];
+    $tax = $subtotal * 0.13;
+    $total = $subtotal + $tax;
+
+    echo "<h3>Order Summary:</h3>";
+    echo "Pizza Size: " . ($size == 'L' ? "Large" : "Extra Large") . "<br>";
+    echo "Number of Toppings: " . $toppings . "<br>";
+    echo "Subtotal: $" . number_format($subtotal, 2) . "<br>";
+    echo "Tax (13%): $" . number_format($tax, 2) . "<br>";
+    echo "Total: $" . number_format($total, 2);
+  } else {
+    echo "Please submit the form.";
+  }
+ ?>
+</body>
+
 </html>
